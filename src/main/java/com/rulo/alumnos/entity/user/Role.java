3c7859cb163ba464b7fca9813fad0ceb8af6b363
ronlucas <ronlucas86@gmail.com>
2018-03-18 20:46:44 -0300

    -agrego roles para autenticacion.

---
+++ src/main/java/com/rulo/alumnos/entity/user/Role.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.rulo.alumnos.entity.user;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author ronlucas
+ *
+ */
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int id;
+	@Column(name = "role")
+	private String role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
